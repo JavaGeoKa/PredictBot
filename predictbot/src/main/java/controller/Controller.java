487870e478531d0351e0234aa539ced3a44693f9
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-12-03 11:24:59 +0300

    v6

--- predictbot/src/main/java/controller/Controller.java
+++ predictbot/src/main/java/controller/Controller.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -9,17 +13,17 @@ import bots.TelegramBot;
 public class Controller {
 
 	public static void main(String[] args) {
-//		System.out.println("Working Directory = " + System.getProperty("user.dir"));
-//		PrintWriter writer;
-//		try {
-//			writer = new PrintWriter("log.txt", "UTF-8");
-//			writer.println("Initialize");
-//			writer.close();
-//
-//		} catch (FileNotFoundException | UnsupportedEncodingException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("log.txt", "UTF-8");
+			writer.println("Initialize");
+			writer.close();
+
+		} catch (FileNotFoundException | UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
 		ApiContextInitializer.init();
 		TelegramBotsApi botsApi = new TelegramBotsApi();
